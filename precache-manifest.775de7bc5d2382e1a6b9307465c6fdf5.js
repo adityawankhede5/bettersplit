e1feaa3d421bc620ca16c107c67e3f2df027d178
adityawankhede5 <adityawankhede555@gmail.com>
2020-06-20 00:35:23 +0000

    Updates

---
+++ precache-manifest.775de7bc5d2382e1a6b9307465c6fdf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9f8f6a33d57d18e74de9a3531397d3",
+    "url": "/bettersplit/index.html"
+  },
+  {
+    "revision": "b83e8ca26f647c8e39f8",
+    "url": "/bettersplit/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2e958b8eda0c79a97453",
+    "url": "/bettersplit/static/css/main.f14b0f4d.chunk.css"
+  },
+  {
+    "revision": "b83e8ca26f647c8e39f8",
+    "url": "/bettersplit/static/js/2.0c5dfc58.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bettersplit/static/js/2.0c5dfc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e958b8eda0c79a97453",
+    "url": "/bettersplit/static/js/main.17dcf879.chunk.js"
+  },
+  {
+    "revision": "3c2b7d8a62aad5a9accd",
+    "url": "/bettersplit/static/js/runtime-main.2dcffe5e.js"
+  }
+]);
